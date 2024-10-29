eeef9077c2bc67acd82a82948f5d1969096f6cd5
JSW0721 <stmjdi04@gmail.com>
2024-10-29 11:06:55 +0900

    Add files via upload
    Entity commit

---
+++ entity/Comment.java
@@ -0,0 +1,39 @@
+package DTO.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "comment")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id",unique = true ,nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @ColumnDefault("1")
+    private int type; //게시판 속성
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+}
