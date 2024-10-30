fce38418f31b7561a2fc65fe04990760ab7b4169
wonsun2006 <wonsun2006@gmail.com>
2024-10-31 02:13:12 +0900

    chore: 프로젝트 구조 변경
    Spring 프로젝트 상위 디렉터리로 이동
    .gitignore 추가
    테스트 파일 삭제

--- src/main/java/com/estsoft/guesshangeul/GuessHangeulApplication.java
+++ src/main/java/com/estsoft/guesshangeul/GuessHangeulApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuessHangeulApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GuessHangeulApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GuessHangeulApplication.class, args);
+	}
 
 }
