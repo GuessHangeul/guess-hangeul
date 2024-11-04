9a2019012c03928978321ffb3893061d1bf3d9f8
wonsun2006 <wonsun2006@gmail.com>
2024-11-04 10:37:40 +0900

    fix: PR 충돌 해결

--- src/main/java/com/estsoft/guesshangeul/comment/entity/GeneralComment.java
+++ src/main/java/com/estsoft/guesshangeul/comment/entity/GeneralComment.java
@@ -2,15 +2,14 @@ package com.estsoft.guesshangeul.comment.entity;
 
 import java.time.LocalDateTime;
 
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import com.estsoft.guesshangeul.post.entity.GeneralPost;
+import com.estsoft.guesshangeul.user.entity.Users;
 
 import com.estsoft.guesshangeul.post.entity.GeneralPost;
 import com.estsoft.guesshangeul.user.entity.Users;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,11 +25,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "general_comment")
-@EntityListeners(AuditingEntityListener.class)
 public class GeneralComment {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "general_comment_id")
+	@Column(name = "general_comment_id", unique = true, nullable = false)
 	private Long id;
 
 	@ManyToOne
@@ -44,7 +42,6 @@ public class GeneralComment {
 	@Column(nullable = false)
 	private String content;
 
-	@CreatedDate
 	@Column(nullable = false)
 	private LocalDateTime createdAt;
 }
