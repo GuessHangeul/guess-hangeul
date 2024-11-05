78284c3ae813360ee554fd61e8cde3a4a3bb1761
gmlwn77 <joo3406@gmail.com>
2024-11-05 13:37:05 +0900

    feat: "게시판 삭제 추가"
    일반 게시판 삭제 추가

--- src/main/java/com/estsoft/guesshangeul/admin/controller/AdminController.java
+++ src/main/java/com/estsoft/guesshangeul/admin/controller/AdminController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estsoft.guesshangeul.admin.service.AdminService;
+import com.estsoft.guesshangeul.board.dto.GeneralBoardResponse;
+import com.estsoft.guesshangeul.board.entity.GeneralBoard;
 import com.estsoft.guesshangeul.user.dto.UsersResponse;
 import com.estsoft.guesshangeul.user.entity.Users;
 
@@ -21,6 +23,12 @@ public class AdminController {
 	public ResponseEntity<UsersResponse> resetNickname(@PathVariable Long userId) {
 		Users users = adminService.resetNickname(userId);
 		return ResponseEntity.ok(new UsersResponse(users));
+	}
+
+	@GetMapping("/api/admin/deleteBoard/quizBoard/{board_id}")
+	public ResponseEntity<GeneralBoardResponse> deleteGeneralBoard(@PathVariable Long board_id) {
+		GeneralBoard generalBoard = adminService.deleteGeneralBoard(board_id);
+		return ResponseEntity.ok(new GeneralBoardResponse(generalBoard));
 	}
 
 }
--- src/main/java/com/estsoft/guesshangeul/admin/service/AdminService.java
+++ src/main/java/com/estsoft/guesshangeul/admin/service/AdminService.java
@@ -3,6 +3,8 @@ package com.estsoft.guesshangeul.admin.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.estsoft.guesshangeul.board.entity.GeneralBoard;
+import com.estsoft.guesshangeul.board.repository.GeneralBoardRepository;
 import com.estsoft.guesshangeul.user.entity.Users;
 import com.estsoft.guesshangeul.user.repository.UsersRepository;
 
@@ -10,11 +12,19 @@ import com.estsoft.guesshangeul.user.repository.UsersRepository;
 public class AdminService {
 	@Autowired
 	UsersRepository usersRepository;
+	@Autowired
+	private GeneralBoardRepository generalBoardRepository;
 
 	public Users resetNickname(Long userId) {
 		Users users = usersRepository.findById(userId).orElseThrow();
 		users.setNickname("hangeul_" + users.getId());
 		return usersRepository.save(users);
+	}
+
+	public GeneralBoard deleteGeneralBoard(Long boardId) {
+		GeneralBoard generalBoard = generalBoardRepository.findById(boardId).orElseThrow();
+		generalBoard.setIsDeleted(true);
+		return generalBoardRepository.save(generalBoard);
 	}
 
 }
