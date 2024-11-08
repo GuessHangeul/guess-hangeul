e09582184949278e2383c4cc9fc52a37f78d4756
gmlwn77 <joo3406@gmail.com>
2024-11-08 09:32:38 +0900

    feat: "어드민 일반 게시판 페이지 추가"
    페이지 기본 틀 작성
    
    게시글 조회, 검색 api 완료 시 기능 추가 예정
    
    # Conflicts:
    #	src/main/java/com/estsoft/guesshangeul/admin/controller/AdminPageController.java
    #	src/main/resources/templates/common/sidebar.html

--- src/main/java/com/estsoft/guesshangeul/admin/controller/AdminPageController.java
+++ src/main/java/com/estsoft/guesshangeul/admin/controller/AdminPageController.java
@@ -2,20 +2,28 @@ package com.estsoft.guesshangeul.admin.controller;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.estsoft.guesshangeul.admin.service.AdminBoardService;
+import com.estsoft.guesshangeul.board.dto.GeneralBoardDto;
+import com.estsoft.guesshangeul.board.dto.GeneralBoardResponse;
+import com.estsoft.guesshangeul.board.service.GeneralBoardService;
+import com.estsoft.guesshangeul.board.service.QuizBoardService;
 import com.estsoft.guesshangeul.user.dto.UsersResponse;
 
 @Controller
 public class AdminPageController {
 
 	private final AdminBoardService adminBoardService;
+	private final GeneralBoardService generalBoardService;
 
-	public AdminPageController(AdminBoardService adminBoardService) {
+	public AdminPageController(AdminBoardService adminBoardService, GeneralBoardService generalBoardService,) {
 		this.adminBoardService = adminBoardService;
+		this.generalBoardService = generalBoardService;
 	}
 
 	@GetMapping("/admin")
@@ -25,5 +33,17 @@ public class AdminPageController {
 		model.addAttribute("users", usersResponses);
 
 		return "admin";
+	}
+
+	@GetMapping("/admin/generalBoard/{boardId}")
+	public String showGeneralBoard(@PathVariable String boardId, Model model, Pageable pageable) {
+		// generalBoard 목록 조회
+		List<GeneralBoardDto> result = generalBoardService.findAllGeneralBoardByIsDeleted(false, pageable);
+		List<GeneralBoardResponse> boardResponses = result.stream().map(GeneralBoardResponse::new).toList();
+		model.addAttribute("generalBoard", boardResponses);
+
+		// generalPost 조회
+
+		return "adminGeneralBoard";
 	}
 }
