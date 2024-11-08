d1516823d8e6f18ab11149a9b564344af6cd333d
gmlwn77 <joo3406@gmail.com>
2024-11-08 14:35:21 +0900

    fix: "어드민 일반 게시판 페이지 수정"
    게시판 title, 삭제 추가
    
    게시글 조회, 검색 api 완료 시 기능 추가 예정

--- src/main/java/com/estsoft/guesshangeul/admin/controller/AdminPageController.java
+++ src/main/java/com/estsoft/guesshangeul/admin/controller/AdminPageController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.estsoft.guesshangeul.admin.service.AdminBoardService;
 import com.estsoft.guesshangeul.board.dto.GeneralBoardDto;
 import com.estsoft.guesshangeul.board.dto.GeneralBoardResponse;
+import com.estsoft.guesshangeul.board.dto.QuizBoardDto;
+import com.estsoft.guesshangeul.board.dto.QuizBoardResponse;
 import com.estsoft.guesshangeul.board.service.GeneralBoardService;
 import com.estsoft.guesshangeul.board.service.QuizBoardService;
 import com.estsoft.guesshangeul.user.dto.UsersResponse;
@@ -37,6 +39,8 @@ public class AdminPageController {
 
 	@GetMapping("/admin/generalBoard/{boardId}")
 	public String showGeneralBoard(@PathVariable String boardId, Model model, Pageable pageable) {
+		model.addAttribute("board", generalBoardService.findByBoardId(Long.parseLong(boardId)));
+
 		// generalBoard 목록 조회
 		List<GeneralBoardDto> result = generalBoardService.findAllGeneralBoardByIsDeleted(false, pageable);
 		List<GeneralBoardResponse> boardResponses = result.stream().map(GeneralBoardResponse::new).toList();
@@ -45,5 +49,17 @@ public class AdminPageController {
 		// generalPost 조회
 
 		return "adminGeneralBoard";
+	}
+
+	@GetMapping("/admin/quizBoard/{boardId}")
+	public String showQuizBoard(@PathVariable String boardId, Model model, Pageable pageable) {
+		// quizBoard 목록 조회
+		List<QuizBoardDto> result = quizBoardService.findAllQuizBoardByIsDeleted(false, pageable);
+		List<QuizBoardResponse> response = result.stream().map(QuizBoardResponse::new).toList();
+		model.addAttribute("quizBoard", response);
+
+		// quizPost 조회
+
+		return "adminQuizBoard";
 	}
 }
