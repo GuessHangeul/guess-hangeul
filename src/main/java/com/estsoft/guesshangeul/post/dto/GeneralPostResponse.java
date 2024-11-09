f56c6cb9fde6e0a8cc862be52d250583e01c43aa
gmlwn77 <joo3406@gmail.com>
2024-11-09 22:12:24 +0900

    fix: "일반 게시판 boardId별 조회 수정"
    일반 게시판 전체 조회 boardId 추가

--- src/main/java/com/estsoft/guesshangeul/post/dto/GeneralPostResponse.java
+++ src/main/java/com/estsoft/guesshangeul/post/dto/GeneralPostResponse.java
@@ -1,39 +1,38 @@
 package com.estsoft.guesshangeul.post.dto;
 
-import com.estsoft.guesshangeul.board.entity.GeneralBoard;
+import java.time.LocalDateTime;
+
 import com.estsoft.guesshangeul.post.entity.GeneralPost;
-import com.estsoft.guesshangeul.user.entity.Users;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 public class GeneralPostResponse {
-    private Long id;
-    private Users users;
-    private GeneralBoard generalBoard;
-    private String title;
-    private String content;
-    private boolean isHidden;
-    private Long view;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+	private Long id;
+	private Long userId;
+	private Long generalBoardId;
+	private String title;
+	private String content;
+	private boolean isHidden;
+	private Long view;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
 
-    public GeneralPostResponse(GeneralPost generalPost) {
-        this.id = generalPost.getId();
-        this.users = generalPost.getUsers();
-        this.generalBoard = generalPost.getGeneralBoard();
-        this.title = generalPost.getTitle();
-        this.content = generalPost.getContent();
-        this.isHidden = generalPost.isHidden();
-        this.view = generalPost.getView();
-        this.createdAt = generalPost.getCreatedAt();
-        this.updatedAt = generalPost.getUpdatedAt();
-    }
+	public GeneralPostResponse(GeneralPost generalPost) {
+		this.id = generalPost.getId();
+		this.userId = generalPost.getUsers().getId();
+		this.generalBoardId = generalPost.getGeneralBoard().getId();
+		this.title = generalPost.getTitle();
+		this.content = generalPost.getContent();
+		this.isHidden = generalPost.isHidden();
+		this.view = generalPost.getView();
+		this.createdAt = generalPost.getCreatedAt();
+		this.updatedAt = generalPost.getUpdatedAt();
+	}
 }
