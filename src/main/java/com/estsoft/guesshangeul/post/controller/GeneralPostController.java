d3cdf6353bfab46085db2d132936a6c98bd2ec4a
wonsun2006 <wonsun2006@gmail.com>
2024-11-11 13:58:19 +0900

    fix: 게시글 생성 body 수정
    게시글 생성 시 Users, GeneralBoard 정보 모두 입력해야 하는 문제점
    - Users는 Security Context에서 연동
    - GeneralBoard는 generalBoardId로 정보 연동

--- src/main/java/com/estsoft/guesshangeul/post/controller/GeneralPostController.java
+++ src/main/java/com/estsoft/guesshangeul/post/controller/GeneralPostController.java
@@ -1,57 +1,73 @@
 package com.estsoft.guesshangeul.post.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.estsoft.guesshangeul.post.dto.AddGeneralPostRequest;
 import com.estsoft.guesshangeul.post.dto.GeneralPostResponse;
 import com.estsoft.guesshangeul.post.dto.UpdateGeneralPostRequest;
 import com.estsoft.guesshangeul.post.service.GeneralPostService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
-@RequestMapping("/api/generalBoard/{general_board_id}/generalPost")
+@RequestMapping("/api/generalBoard/{generalBoardId}/generalPost")
 public class GeneralPostController {
-    private final GeneralPostService generalPostService;
+	private final GeneralPostService generalPostService;
 
-    public GeneralPostController(GeneralPostService generalPostService) {
-        this.generalPostService = generalPostService;
-    }
-    // 전체 게시글 조회
-    @GetMapping
-    public ResponseEntity<List<GeneralPostResponse>> getAllGeneralPosts() {
-        List<GeneralPostResponse> posts = generalPostService.getAllGeneralPosts();
-        return ResponseEntity.status(HttpStatus.OK).body(posts);
-    }
-    // 게시글 id로 조회
-    @GetMapping("/{general_post_id}")
-    public ResponseEntity<GeneralPostResponse> getGeneralPostById(@PathVariable Long general_post_id) {
-        GeneralPostResponse post = generalPostService.getGeneralPostById(general_post_id);
-        return ResponseEntity.status(HttpStatus.OK).body(post);
-    }
-    // 게시글 제목으로 조회
-    @GetMapping("?search={title}")
-    public ResponseEntity<GeneralPostResponse> getGeneralPostByTitle(@PathVariable String title) {
-        GeneralPostResponse post = generalPostService.getGeneralPostByTitle(title);
-        return ResponseEntity.status(HttpStatus.OK).body(post);
-    }
-    // 게시글 생성
-    @PostMapping
-    public ResponseEntity<GeneralPostResponse> createGeneralPost(@RequestBody AddGeneralPostRequest request) {
-        GeneralPostResponse post = generalPostService.createGeneralPost(request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(post);
-    }
-    // 게시글 수정
-    @PutMapping("/{general_post_id}")
-    public ResponseEntity<GeneralPostResponse> updateGeneralPost(@PathVariable Long general_post_id, @RequestBody UpdateGeneralPostRequest request) {
-        GeneralPostResponse post = generalPostService.updateGeneralPost(general_post_id, request);
-        return ResponseEntity.status(HttpStatus.OK).body(post);
-    }
-    // 게시글 삭제
-    @DeleteMapping("/{general_post_id}")
-    public ResponseEntity<Void> deleteGeneralPost(@PathVariable Long general_post_id) {
-        generalPostService.deleteGeneralPost(general_post_id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-    }
+	public GeneralPostController(GeneralPostService generalPostService) {
+		this.generalPostService = generalPostService;
+	}
+
+	// 전체 게시글 조회
+	@GetMapping
+	public ResponseEntity<List<GeneralPostResponse>> getAllGeneralPosts() {
+		List<GeneralPostResponse> posts = generalPostService.getAllGeneralPosts();
+		return ResponseEntity.status(HttpStatus.OK).body(posts);
+	}
+
+	// 게시글 id로 조회
+	@GetMapping("/{general_post_id}")
+	public ResponseEntity<GeneralPostResponse> getGeneralPostById(@PathVariable Long general_post_id) {
+		GeneralPostResponse post = generalPostService.getGeneralPostById(general_post_id);
+		return ResponseEntity.status(HttpStatus.OK).body(post);
+	}
+
+	// 게시글 제목으로 조회
+	@GetMapping("?search={title}")
+	public ResponseEntity<GeneralPostResponse> getGeneralPostByTitle(@PathVariable String title) {
+		GeneralPostResponse post = generalPostService.getGeneralPostByTitle(title);
+		return ResponseEntity.status(HttpStatus.OK).body(post);
+	}
+
+	// 게시글 생성
+	@PostMapping
+	public ResponseEntity<GeneralPostResponse> createGeneralPost(@RequestBody AddGeneralPostRequest request,
+		@PathVariable Long generalBoardId) {
+		GeneralPostResponse post = generalPostService.createGeneralPost(request, generalBoardId);
+		return ResponseEntity.status(HttpStatus.CREATED).body(post);
+	}
+
+	// 게시글 수정
+	@PutMapping("/{general_post_id}")
+	public ResponseEntity<GeneralPostResponse> updateGeneralPost(@PathVariable Long general_post_id,
+		@RequestBody UpdateGeneralPostRequest request) {
+		GeneralPostResponse post = generalPostService.updateGeneralPost(general_post_id, request);
+		return ResponseEntity.status(HttpStatus.OK).body(post);
+	}
+
+	// 게시글 삭제
+	@DeleteMapping("/{general_post_id}")
+	public ResponseEntity<Void> deleteGeneralPost(@PathVariable Long general_post_id) {
+		generalPostService.deleteGeneralPost(general_post_id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 }
