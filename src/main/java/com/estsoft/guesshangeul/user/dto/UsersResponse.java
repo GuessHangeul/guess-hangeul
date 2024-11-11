92e9615a0c8978cf8e08a476073ebe825bdbdc2a
gmlwn77 <joo3406@gmail.com>
2024-11-11 13:59:42 +0900

    fix: "유저 조회 날짜 데이터 형식 변경"
    UsersResponse - connectedAt, createdAt 형식 변경

--- src/main/java/com/estsoft/guesshangeul/user/dto/UsersResponse.java
+++ src/main/java/com/estsoft/guesshangeul/user/dto/UsersResponse.java
@@ -1,9 +1,8 @@
 package com.estsoft.guesshangeul.user.dto;
 
-import java.time.LocalDateTime;
+import static com.estsoft.guesshangeul.util.DateFormatUtil.*;
 
 import com.estsoft.guesshangeul.user.entity.Users;
-import com.estsoft.guesshangeul.user.service.UsersDetailsService;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,26 +13,26 @@ public class UsersResponse {
 	private Long userId;
 	private String nickname;
 	private String authority;
-	private LocalDateTime createdAt;
-	private LocalDateTime connectedAt;
+	private String createdAt;
+	private String connectedAt;
 	private int connectCount;
 	private boolean isDeleted;
-
 
 	public UsersResponse(Users users) {
 		this.userId = users.getId();
 		this.nickname = users.getNickname();
-		this.createdAt = users.getCreatedAt();
-		this.connectedAt = users.getConnectedAt();
+		this.createdAt = users.getCreatedAt().format(formatter);
+		this.connectedAt = users.getConnectedAt().format(formatter);
 		this.connectCount = users.getConnectCount();
 		this.isDeleted = users.isDeleted();
 	}
-	public UsersResponse(Users users,String authorityString) {
+
+	public UsersResponse(Users users, String authorityString) {
 		this.userId = users.getId();
 		this.nickname = users.getNickname();
 		this.authority = authorityString;
-		this.createdAt = users.getCreatedAt();
-		this.connectedAt = users.getConnectedAt();
+		this.createdAt = users.getCreatedAt().format(formatter);
+		this.connectedAt = users.getConnectedAt().format(formatter);
 		this.connectCount = users.getConnectCount();
 		this.isDeleted = users.isDeleted();
 	}
