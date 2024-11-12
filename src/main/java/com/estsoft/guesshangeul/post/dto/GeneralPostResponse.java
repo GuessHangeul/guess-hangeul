a802cac4cf110297b1fa1684cf383a832e1313de
gmlwn77 <joo3406@gmail.com>
2024-11-12 16:38:13 +0900

    feat: "response @Schema 추가"
    @Schema 추가

--- src/main/java/com/estsoft/guesshangeul/post/dto/GeneralPostResponse.java
+++ src/main/java/com/estsoft/guesshangeul/post/dto/GeneralPostResponse.java
@@ -4,6 +4,7 @@ import static com.estsoft.guesshangeul.util.DateFormatUtil.*;
 
 import com.estsoft.guesshangeul.post.entity.GeneralPost;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,16 +14,27 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Schema(description = "일반 게시글 응답")
 public class GeneralPostResponse {
+	@Schema(description = "게시글 ID", type = "Long")
 	private Long id;
+	@Schema(description = "유저 ID", type = "Long")
 	private Long userId;
+	@Schema(description = "유저 닉네임", type = "String")
 	private String nickname;
+	@Schema(description = "일반 게시판 ID", type = "Long")
 	private Long generalBoardId;
+	@Schema(description = "게시글 제목", type = "String")
 	private String title;
+	@Schema(description = "게시글 내용", type = "String")
 	private String content;
+	@Schema(description = "게시글 숨김 여부", type = "boolean")
 	private boolean isHidden;
+	@Schema(description = "게시글 조회수", type = "Long")
 	private Long view;
+	@Schema(description = "게시글 생성 날짜", type = "String")
 	private String createdAt;
+	@Schema(description = "게시글 업데이트 날짜", type = "String")
 	private String updatedAt;
 
 	public GeneralPostResponse(GeneralPost generalPost) {
--- src/main/java/com/estsoft/guesshangeul/user/dto/UsersResponse.java
+++ src/main/java/com/estsoft/guesshangeul/user/dto/UsersResponse.java
@@ -4,18 +4,27 @@ import static com.estsoft.guesshangeul.util.DateFormatUtil.*;
 
 import com.estsoft.guesshangeul.user.entity.Users;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "유저 응답")
 public class UsersResponse {
+	@Schema(description = "유저 ID", type = "Long")
 	private Long userId;
+	@Schema(description = "유저 닉네임", type = "String")
 	private String nickname;
+	@Schema(description = "유저 권한", type = "String")
 	private String authority;
+	@Schema(description = "유저 생성 날짜", type = "String")
 	private String createdAt;
+	@Schema(description = "유저 최근 접속 날짜", type = "String")
 	private String connectedAt;
+	@Schema(description = "유저 접속 횟수", type = "int")
 	private int connectCount;
+	@Schema(description = "유저 삭제 여부", type = "boolean")
 	private boolean isDeleted;
 
 	public UsersResponse(Users users) {
