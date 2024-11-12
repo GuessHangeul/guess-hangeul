dc988836f526a736c555e10bf8ab9b5eff484fe6
wonsun2006 <wonsun2006@gmail.com>
2024-11-12 16:22:34 +0900

    feat: board 도메인 Swagger 적용
    - 일반 게시판 목록 조회
    - 문제 게시판 목록 조회
    - 전체 게시판 조회
    - 문제 게시판 생성

--- src/main/java/com/estsoft/guesshangeul/board/dto/QuizBoardResponse.java
+++ src/main/java/com/estsoft/guesshangeul/board/dto/QuizBoardResponse.java
@@ -2,14 +2,19 @@ package com.estsoft.guesshangeul.board.dto;
 
 import com.estsoft.guesshangeul.board.entity.QuizBoard;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "문제 게시판 응답")
 public class QuizBoardResponse {
+	@Schema(description = "게시판 ID", type = "Long")
 	private Long id;
+	@Schema(description = "게시판 제목", type = "String")
 	private String title;
+	@Schema(description = "게시판 삭제 여부", type = "Boolean")
 	private Boolean isDeleted;
 
 	public QuizBoardResponse(QuizBoardDto quizBoard) {
