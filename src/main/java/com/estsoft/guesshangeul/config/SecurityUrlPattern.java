2bb334c25ccac3ad194d5a9b910607543a9ac197
junsik2 <arc551234@gmail.com>
2024-11-13 23:15:41 +0900

    refactor : SecurityUrlPattern 메소드 구분

--- src/main/java/com/estsoft/guesshangeul/config/SecurityUrlPattern.java
+++ src/main/java/com/estsoft/guesshangeul/config/SecurityUrlPattern.java
@@ -1,47 +1,46 @@
 package com.estsoft.guesshangeul.config;
 
+import org.springframework.http.HttpMethod;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public enum SecurityUrlPattern {
-	// 모든 사용자가 접근 가능한 URL 패턴
-	// PUBLIC(new String[] {
-	// 	"/api/public/"
-	// }, null),
+	// 로그인 하지 않은 사용자
+	LOGIN(HttpMethod.POST, "/api/login", "ANONYMOUS"),
+	SIGNUP(HttpMethod.POST, "/api/signup", "ANONYMOUS"),
+	LOGIN_PAGE(HttpMethod.GET, "/login", "ANONYMOUS"),
+	SIGNUP_PAGE(HttpMethod.GET, "/signup", "ANONYMOUS"),
+	FIND_USER_PAGE(HttpMethod.GET, "/findUser", "ANONYMOUS"),
+	CHECK_EMAIL(HttpMethod.POST, "/api/checkEmailDuplicate", "ANONYMOUS"),
+	CHECK_NICKNAME(HttpMethod.POST, "/api/checkNicknameDuplicate", "ANONYMOUS"),
+	RESET_PASSWORD_REQUEST(HttpMethod.POST, "/api/resetPasswordRequest/{email}", "ANONYMOUS"),
+	RESET_PASSWORD_PAGE(HttpMethod.GET, "/resetPassword/{token}", "ANONYMOUS"),
+	RESET_PASSWORD(HttpMethod.POST, "/api/resetPassword", "ANONYMOUS"),
 
-	// 로그인 하지 않은 사용자 URL
-	ANONYMOUS(new String[] {
-		"signup", "api/signup", "login", "api/login", "findUser",
-		"api/resetPassword", "api/resetPasswordRequest/"
-	}, "ANONYMOUS"),
+	// 노비 권한
+	SELF_WITHDRAWAL(HttpMethod.PUT, "/api/selfWithdrawal", "NOBI"),
+	LOGOUT(HttpMethod.GET, "/api/logout", "NOBI"),
+	FIND_AUTHORITY(HttpMethod.GET, "api/user/findAuthority/{userId}", "NOBI"),
+	ADD_AUTHORITY(HttpMethod.POST, "api/user/authority", "NOBI"),
+	DELETE_AUTHORITY(HttpMethod.DELETE, "api/user/authority", "NOBI"),
 
-	// 노비 권한 URL
-	NOBI(new String[] {
-		"/api/selfWithdrawal", "api/user/findAuthority/"
-	}, "NOBI"),
+	// 평민 권한
+	PYEONGMIN_API(HttpMethod.GET, "/api/pyeongmin/**", "PYEONGMIN"),
 
-	// 평민 권한 URL
-	PYEONGMIN(new String[] {
-		"/api/pyeongmin/"
-	}, "PYEONGMIN"),
+	// 양반 권한
+	YANGBAN_API(HttpMethod.GET, "/api/yangban/**", "YANGBAN"),
 
-	// 양반 권한 URL
-	YANGBAN(new String[] {
-		"/api/yangban/"
-	}, "YANGBAN"),
+	// 집현전 권한
+	JIPHYEONJEON_API(HttpMethod.PUT, "/api/jiphyeonjeon/**", "JIPHYEONJEON"),
 
-	// 집현전 권한 URL
-	JIPHYEONJEON(new String[] {
-		"/api/jiphyeonjeon/"
-	}, "JIPHYEONJEON"),
+	// 세종대왕 권한
+	ADMIN_API(HttpMethod.PUT, "/api/admin/**", "KINGSEJONG"),
+	ADMIN_PAGE(HttpMethod.GET, "/admin/**", "KINGSEJONG");
 
-	// 세종대왕 권한 URL
-	KINGSEJONG(new String[] {
-		"/api/withDrawal/", "api/user/authority"
-	}, "KINGSEJONG");
-
-	private final String[] patterns;
+	private final HttpMethod method;
+	private final String pattern;
 	private final String authority;
 }
