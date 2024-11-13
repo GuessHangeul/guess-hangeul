a83e9038a8cfa6bbfd81a0d3b7732b46a04332a4
gmlwn77 <joo3406@gmail.com>
2024-11-14 00:27:25 +0900

    fix: "집현전 신청 수정"
    집현전 신청자는 신청 페이지 접근 불가하게 수정

--- src/main/java/com/estsoft/guesshangeul/userrank/controller/RankingPageController.java
+++ src/main/java/com/estsoft/guesshangeul/userrank/controller/RankingPageController.java
@@ -1,8 +1,16 @@
 package com.estsoft.guesshangeul.userrank.controller;
 
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.estsoft.guesshangeul.admin.entity.BoardManagerApply;
+import com.estsoft.guesshangeul.user.entity.Users;
+import com.estsoft.guesshangeul.user.service.UsersDetailsService;
 import com.estsoft.guesshangeul.userrank.service.BoardManagerApplyService;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 public class RankingPageController {
 	private final BoardManagerApplyService service;
 	private final BoardManagerApplyService boardManagerApplyService;
+	private final UsersDetailsService usersDetailsService;
 
 	@GetMapping("/rank")
 	public String showRankingPage() {
@@ -20,6 +29,22 @@ public class RankingPageController {
 
 	@GetMapping("/newBoardManagerApply")
 	public String applyBoardManager() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String username;
+		if (authentication.getPrincipal() instanceof UserDetails userDetails) {
+			username = userDetails.getUsername();
+		} else {
+			username = (String)authentication.getPrincipal();
+		}
+
+		Users users = (Users)usersDetailsService.loadUserByUsername(username);
+		List<BoardManagerApply> responseList = service.findAll();
+
+		for (BoardManagerApply boardManagerApply : responseList) {
+			if (boardManagerApply.getUsers().getId() == users.getId()) {
+				return "NoBoardManager";
+			}
+		}
 		return "NewBoardManagerApply";
 	}
 }
