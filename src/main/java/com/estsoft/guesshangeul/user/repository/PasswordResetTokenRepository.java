825673768e919810cf13b5a3b86405e8cedadabd
junsik2 <arc551234@gmail.com>
2024-11-14 15:23:53 +0900

    refactor : 계정 찾기 오류 수정

--- src/main/java/com/estsoft/guesshangeul/user/repository/PasswordResetTokenRepository.java
+++ src/main/java/com/estsoft/guesshangeul/user/repository/PasswordResetTokenRepository.java
@@ -4,7 +4,6 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.estsoft.guesshangeul.user.entity.PasswordResetToken;
 import com.estsoft.guesshangeul.user.entity.Users;
@@ -13,6 +12,5 @@ import com.estsoft.guesshangeul.user.entity.Users;
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
 	Optional<PasswordResetToken> findByToken(String token);
 
-	@Transactional(rollbackFor = Exception.class)
 	void deleteByUser(Users user);
 }
